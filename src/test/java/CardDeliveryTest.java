45b5f20e0e81d201d60c1282c5a10d50fd3b8819
888KEKC888 <sitneckov@mail.ru>
2025-07-02 07:53:52 +0300

    change test, delete wrong test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -63,35 +63,4 @@ public class CardDeliveryTest {
 
     }
 
-    @Test
-    void testSuccessfulReplanFail() {
-        var daysFirstMeeting = 4;
-        var daysSecondMeeting = 5;
-
-        $("[data-test-id='city'] .input__control").setValue(DataGenerator.generateCity("ru"));
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(DataGenerator
-                .generateData(daysFirstMeeting, "dd.MM.yyyy"));
-        $("[data-test-id='name'] [name='name']").setValue(DataGenerator.generateName("en"));
-        $("[data-test-id='phone'] [name='phone']").setValue(DataGenerator.generatePhone("ru"));
-        $("[data-test-id='agreement']").click();
-        $(".button").click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на "
-                        + DataGenerator.generateData(daysFirstMeeting, "dd.MM.yyyy")), Duration.ofSeconds(15))
-                .shouldBe(visible);
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(DataGenerator
-                .generateData(daysSecondMeeting, "dd.MM.yyyy"));
-        $(".button").click();
-        $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                .shouldBe(visible);
-        $("[data-test-id='replan-notification'] .button__text").click();
-        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + DataGenerator.generateData(daysSecondMeeting, "dd.MM.yyyy")));
-
-    }
 }
